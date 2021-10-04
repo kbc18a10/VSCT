3d0ce9b3b77399538ea1284524bfc0c5d7431bbf
kbc18a12 <kbc18a12@stu.kawahara.ac.jp>
2021-10-04 14:23:28 +0900

    とりあえず仮置き

---
+++ home.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../css/aaa.css';
+import { Link } from 'react-router-dom'
+
+
+
+const Index = () => {
+
+  return (
+
+    <div>
+      <h1>VSCT</h1>
+      <Link to="/" style={{ textDecoration: "none" }}><div className="link">Play</div></Link>
+      
+    </div>
+
+
+
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Index
