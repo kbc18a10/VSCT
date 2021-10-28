0cce3a35222385da03bb45ae950ea7108ac81d3d
kbc18a12 <kbc18a12@stu.kawahara.ac.jp>
2021-10-28 11:20:38 +0900

    今日の分こみ

--- front/src/pages/Game.jsx
+++ front/src/pages/Game.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import Gamemasu from "../image/Gamemasu.png"
+import "../Game.css"
+
 
 const Game = () => {
     return (
         <div className="Game"> 
-            <h1>ゲーム画面</h1>
+        <p>player1:</p>
+            <div id="back" >
+               Time
+            </div> 
         </div>
     );
 }
